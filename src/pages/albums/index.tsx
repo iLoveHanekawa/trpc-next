debf8fe0a531f1df13c008973aca4a02ca9d0e73
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-11 15:27:45 +0530

    del album form;

--- src/pages/albums/index.tsx
+++ src/pages/albums/index.tsx
@@ -5,13 +5,15 @@ import Link from "next/link"
 type Album = {
     title: string
     artistId: string
+    delId: string
 }
 
 export default function AlbumsPage() {
 
-    const [album, setAlbum] = useState<Album>({ title: '', artistId: '' })
+    const [album, setAlbum] = useState<Album>({ title: '', artistId: '', delId: '' })
 
     const addMutation = client.album.create.useMutation()
+    const delMutation = client.album.delete.useMutation()
     const { data } = client.album.findmany.useQuery()
 
     return <div className = 'flex flex-col mx-3 mt-3'>
@@ -40,7 +42,7 @@ export default function AlbumsPage() {
     <form onSubmit={(e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         addMutation.mutate({ title: album.title, artistId: album.artistId })
-        setAlbum({ title: '', artistId: '' })
+        setAlbum({ ...album, title: '', artistId: ''})
     }}>
         <input placeholder="Title" value = {album.title} onChange = {event => {
             setAlbum({...album, title: event.currentTarget.value})
@@ -49,6 +51,16 @@ export default function AlbumsPage() {
             setAlbum({...album, artistId: event.currentTarget.value})
         }} className = 'border-2 py-1 border-gray-300 rounded-md mr-2 indent-2' />
         <button className = 'bg-blue-300 text-white text-sm rounded-full px-4 py-1'>Add Album</button>
+    </form>
+    <form onSubmit = {(event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        delMutation.mutate({ id: album.delId })
+        setAlbum({ ...album, delId: '' })
+    }} className = 'mt-2'>
+        <input placeholder="Delete Id" value = {album.delId} onChange = {event => {
+            setAlbum({...album, delId: event.currentTarget.value})
+        }} className = 'border-2 py-1 border-gray-300 rounded-md mr-2 indent-2' />
+        <button className = 'bg-red-300 text-white text-sm rounded-full px-4 py-1'>Delete Album</button>
     </form>
 </div>
 }
